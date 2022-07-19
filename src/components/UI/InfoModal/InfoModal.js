bf0f7b1a901a733d42dbef232b9bd6b5bd12d420
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2022-07-19 23:14:26 +0200

    portal works!

--- src/components/UI/InfoModal/InfoModal.js
+++ src/components/UI/InfoModal/InfoModal.js
@@ -2,21 +2,43 @@ import styles from "./InfoModal.module.css";
 import Card from "../Card/Card";
 import Subtitle from "../Subtitle/Subtitle";
 import Button from "../Button/Button";
+import { createPortal } from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header>
+        <Subtitle>{props.subtitle}</Subtitle>
+      </header>
+      <div>
+        <p>{props.content}</p>
+      </div>
+      <footer>
+        <Button>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const InfoModal = (props) => {
   return (
     <>
-      <Card>
-        <header>
-          <Subtitle>{props.subtitle}</Subtitle>
-        </header>
-        <div>
-          <p>{props.content}</p>
-        </div>
-        <footer>
-          <Button>Ok</Button>
-        </footer>
-      </Card>
+      {createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {createPortal(
+        <ModalOverlay
+          subtitle={props.subtitle}
+          content={props.content}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("modal-root")
+      )}
     </>
   );
 };
