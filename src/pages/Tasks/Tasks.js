4ddba95be8bf0ddd030476e804508db8683b45b7
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2022-07-26 12:16:59 +0200

    add info to tasks

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,4 +1,20 @@
+import Button from "../../components/UI/Button/Button";
+import Title from "../../components/UI/Title/Title";
+import Card from "../../components/UI/Card/Card";
+import { useNavigate } from "react-router-dom";
 const Tasks = (props) => {
-  return "Tasks";
+  const navigate = useNavigate();
+  return (
+    <Card>
+      <Title>Lista zadań "w budowie".</Title>
+      <Button
+        onClick={() => {
+          navigate("/panel");
+        }}
+      >
+        Wróć
+      </Button>
+    </Card>
+  );
 };
 export default Tasks;
